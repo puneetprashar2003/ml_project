5be637b04822c609f67992656ee8ebcd67656fd3
Puneet <puneetprashar2003@gmail.com>
2023-06-15 11:49:11 +0530

    logging and exception

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys 
-
+import logging
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
     file_name=exc_tb.tb_frame.f_code.co_filename
@@ -9,7 +9,14 @@ def error_message_detail(error,error_detail:sys):
 
 class CustomException(Exception):
     def __init__(self,error_message,error_detail:sys):
-        super.__init__(error_message)
+        super().__init__(error_message)
         self.error_message=error_message_detail(error_message,error_detail=error_detail)
     def __str__(self):
         return self.error_message
+
+if __name__=="__main__":
+    try:
+        a=1/0
+    except Exception as e:
+        logging.info("Divided by 0")
+        raise CustomException(e,sys)
